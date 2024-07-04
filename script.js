ac5f12d82e27aca31942c375cedaef493cccef31
OmegaOrbitals <96728724+OmegaOrbitals@users.noreply.github.com>
2024-07-04 11:52:09 +0000

    Added a cursor effect to the canvas

---
+++ script.js
@@ -0,0 +1,39 @@
+const heroCanvas = document.querySelector(".hero-canvas");
+const heroCtx = heroCanvas.getContext("2d");
+
+let points = [];
+let drawnLines = [];
+
+heroCanvas.height = window.innerHeight;
+heroCanvas.width = window.innerWidth;
+
+heroCtx.strokeStyle = "white";
+heroCtx.lineWidth = 1;
+heroCtx.fillStyle = "white";
+
+for(let i = 0; i < (window.innerHeight * window.innerWidth) * 0.0001; i++) {
+  points.push([Math.floor(Math.random() * window.innerWidth), Math.floor(Math.random() * window.innerHeight)]);
+}
+
+document.addEventListener("mousemove", (ev) => {
+  heroCtx.clearRect(0, 0, heroCanvas.width, heroCanvas.height);
+  drawnLines = [];
+
+  points.forEach((point) => {
+    const distance = Math.sqrt((ev.clientX - point[0]) ** 2 + (ev.clientY - point[1]) ** 2);
+    if(distance < 150) {
+      drawnLines.push({
+        start: [ev.clientX, ev.clientY],
+        end: [point[0], point[1]]
+      })
+    }
+  })
+
+  drawnLines.forEach((line) => {
+    heroCtx.strokeStyle = "white";
+    heroCtx.beginPath();
+    heroCtx.moveTo(line.start[0], line.start[1]);
+    heroCtx.lineTo(line.end[0], line.end[1]);
+    heroCtx.stroke();
+  })
+})
