1c5e2153c303c77dfb733ec661c1e22f3a704884
OmegaOrbitals <96728724+OmegaOrbitals@users.noreply.github.com>
2024-07-04 13:01:17 +0000

    Fixed canvas resizing

--- script.js
+++ script.js
@@ -11,15 +11,30 @@ heroCtx.strokeStyle = "white";
 heroCtx.lineWidth = 1;
 heroCtx.fillStyle = "white";
 
-for(let i = 0; i < (window.innerHeight * window.innerWidth) * 0.0001; i++) {
-  points.push([Math.floor(Math.random() * window.innerWidth), Math.floor(Math.random() * window.innerHeight)]);
+function generatePoints() {
+  points = [];
+
+  for(let i = 0; i < (window.innerHeight * window.innerWidth) * 0.0001; i++) {
+    points.push([Math.floor(Math.random() * window.innerWidth), Math.floor(Math.random() * window.innerHeight)]);
+  }
 }
+
+function drawPoints() {
+  points.forEach((point) => {
+    heroCtx.fillRect(point[0] - 0.75, point[1] - 0.75, 1.5, 1.5);
+  })
+}
+
+generatePoints();
+drawPoints();
 
 document.addEventListener("mousemove", (ev) => {
   heroCtx.clearRect(0, 0, heroCanvas.width, heroCanvas.height);
+
   drawnLines = [];
 
   points.forEach((point) => {
+    heroCtx.fillRect(point[0] - 0.75, point[1] - 0.75, 1.5, 1.5);
     const distance = Math.sqrt((ev.clientX - point[0]) ** 2 + (ev.clientY - point[1]) ** 2);
     if(distance < 150) {
       drawnLines.push({
@@ -30,10 +45,20 @@ document.addEventListener("mousemove", (ev) => {
   })
 
   drawnLines.forEach((line) => {
-    heroCtx.strokeStyle = "white";
     heroCtx.beginPath();
     heroCtx.moveTo(line.start[0], line.start[1]);
     heroCtx.lineTo(line.end[0], line.end[1]);
     heroCtx.stroke();
   })
+})
+
+window.addEventListener("resize", () => {
+  heroCanvas.height = window.innerHeight;
+  heroCanvas.width = window.innerWidth;
+  heroCtx.strokeStyle = "white";
+  heroCtx.lineWidth = 1;
+  heroCtx.fillStyle = "white";
+
+  generatePoints();
+  drawPoints();
 })
